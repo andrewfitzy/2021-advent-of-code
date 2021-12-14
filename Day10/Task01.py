5e3d759559b772248bb91d51dffce891b34ae81e
Andrew Fitzpatrick <andrew.fitzpatrick@clicktravel.com>
2021-12-14 09:27:11 +0000

    Day 10 task 2 completed

--- Day10/Task01.py
+++ Day10/Task01.py
@@ -50,3 +50,109 @@ In the above example, an illegal ) was found twice (2*3 = 6 points), an illegal 
 
 Find the first illegal character in each corrupted line of the navigation subsystem. What is the total syntax error score for those errors?
 """
+
+
+"""
+map a character to its score
+"""
+terminators = {
+    ')': {
+        'score': 3,
+        'starter': '('
+    },
+    ']': {
+        'score': 57,
+        'starter': '['
+    },
+    '}': {
+        'score': 1197,
+        'starter': '{'
+    },
+    '>': {
+        'score': 25137,
+        'starter': '<'
+    },
+}
+
+
+def get_input(filename):
+    """
+    Takes a filename and returns a list of co-ordinates from the file
+    """
+    output = []
+    with open(filename) as f:
+        for line in f:
+            tmp_line = line.strip()
+            output.append(tmp_line)
+    return output
+
+
+def get_dodgy_lines(input):
+    """
+    Use a stack to find the lines that are dodgy
+    """
+    dodgy_lines = []
+    for line in input:
+        tmp_line = line.strip()
+        char_list = list(tmp_line)
+
+        sanity_stack = []
+        for char in char_list:
+            if char in terminators:
+                popped_char = sanity_stack.pop()
+                if popped_char != terminators[char]['starter']:
+                    entry = {
+                        'line': line,
+                        'first_dogy_char': char,
+                    }
+                    dodgy_lines.append(entry)
+                    break;
+            else:
+                sanity_stack.append(char)
+    return dodgy_lines
+
+
+def calculate_syntax_error_score(dodgy_lines):
+    """
+    Given an input like:
+    [
+      {
+        line: ['[','(','>'],
+        first_dogy_char: '>'
+      }
+    ]
+
+    This function will determine the cost of the dodgy character per line and sum them together
+    """
+    score = 0
+    for line in dodgy_lines:
+        score += terminators[line['first_dogy_char']]['score']
+    return score
+
+
+if __name__ == '__main__':
+    """
+    
+    """
+    input = get_input('input.txt')
+
+    # input = ['[({(<(())[]>[[{[]{<()<>>',
+    #          '[(()[<>])]({[<{<<[]>>(',
+    #          '{([(<{}[<>[]}>{[]{[(<()>',
+    #          '(((({<>}<{<{<>}{[]{[]{}',
+    #          '[[<[([]))<([[{}[[()]]]',
+    #          '[{[{({}]{}}([{[{{{}}([]',
+    #          '{<[[]]>}<{[{[{[]{()[[[]',
+    #          '[<(<(<(<{}))><([]([]()',
+    #          '<{([([[(<>()){}]>(<<{{',
+    #          '<{([{{}}[<[[[<>{}]]]>[]]']
+
+    print(input)
+
+    dodgy_lines = get_dodgy_lines(input)
+
+    print(dodgy_lines)
+
+    error_score = calculate_syntax_error_score(dodgy_lines)
+
+    print(str(error_score))
