aa705e1473075720be42fc887692e02a304fd122
Andrew Fitzpatrick <andrew.fitzpatrick@clicktravel.com>
2022-01-16 16:01:04 +0000

    Solution for day 12 of AoC

--- Day12/Task02.py
+++ Day12/Task02.py
@@ -1,97 +1,48 @@
 """
---- Day 12: Passage Pathing ---
-With your submarine's subterranean subsystems subsisting suboptimally, the only way you're getting out of this cave anytime soon is by finding a path yourself. Not just a path - the only way to know if you've found the best path is to find all of them.
+--- Part Two ---
+After reviewing the available paths, you realize you might have time to visit a single small cave twice. Specifically, big caves can be visited any number of times, a single small cave can be visited at most twice, and the remaining small caves can be visited at most once. However, the caves named start and end can only be visited exactly once each: once you leave the start cave, you may not return to it, and once you reach the end cave, the path must end immediately.
 
-Fortunately, the sensors are still mostly working, and so you build a rough map of the remaining caves (your puzzle input). For example:
+Now, the 36 possible paths through the first example above are:
 
-start-A
-start-b
-A-c
-A-b
-b-d
-A-end
-b-end
-This is a list of how all of the caves are connected. You start in the cave named start, and your destination is the cave named end. An entry like b-d means that cave b is connected to cave d - that is, you can move between them.
-
-So, the above cave system looks roughly like this:
-
-    start
-    /   \
-c--A-----b--d
-    \   /
-     end
-Your goal is to find the number of distinct paths that start at start, end at end, and don't visit small caves more than once. There are two types of caves: big caves (written in uppercase, like A) and small caves (written in lowercase, like b). It would be a waste of time to visit any small cave more than once, but big caves are large enough that it might be worth visiting them multiple times. So, all paths you find should visit small caves at most once, and can visit big caves any number of times.
-
-Given these rules, there are 10 paths through this example cave system:
-
+start,A,b,A,b,A,c,A,end
+start,A,b,A,b,A,end
+start,A,b,A,b,end
+start,A,b,A,c,A,b,A,end
+start,A,b,A,c,A,b,end
+start,A,b,A,c,A,c,A,end
 start,A,b,A,c,A,end
 start,A,b,A,end
+start,A,b,d,b,A,c,A,end
+start,A,b,d,b,A,end
+start,A,b,d,b,end
 start,A,b,end
+start,A,c,A,b,A,b,A,end
+start,A,c,A,b,A,b,end
+start,A,c,A,b,A,c,A,end
 start,A,c,A,b,A,end
+start,A,c,A,b,d,b,A,end
+start,A,c,A,b,d,b,end
 start,A,c,A,b,end
+start,A,c,A,c,A,b,A,end
+start,A,c,A,c,A,b,end
+start,A,c,A,c,A,end
 start,A,c,A,end
 start,A,end
+start,b,A,b,A,c,A,end
+start,b,A,b,A,end
+start,b,A,b,end
+start,b,A,c,A,b,A,end
+start,b,A,c,A,b,end
+start,b,A,c,A,c,A,end
 start,b,A,c,A,end
 start,b,A,end
+start,b,d,b,A,c,A,end
+start,b,d,b,A,end
+start,b,d,b,end
 start,b,end
-(Each line in the above list corresponds to a single path; the caves visited by that path are listed in the order they are visited and separated by commas.)
+The slightly larger example above now has 103 paths through it, and the even larger example now has 3509 paths through it.
 
-Note that in this cave system, cave d is never visited by any path: to do so, cave b would need to be visited twice (once on the way to cave d and a second time when returning from cave d), and since cave b is small, this is not allowed.
-
-Here is a slightly larger example:
-
-dc-end
-HN-start
-start-kj
-dc-start
-dc-HN
-LN-dc
-HN-end
-kj-sa
-kj-HN
-kj-dc
-The 19 paths through it are as follows:
-
-start,HN,dc,HN,end
-start,HN,dc,HN,kj,HN,end
-start,HN,dc,end
-start,HN,dc,kj,HN,end
-start,HN,end
-start,HN,kj,HN,dc,HN,end
-start,HN,kj,HN,dc,end
-start,HN,kj,HN,end
-start,HN,kj,dc,HN,end
-start,HN,kj,dc,end
-start,dc,HN,end
-start,dc,HN,kj,HN,end
-start,dc,end
-start,dc,kj,HN,end
-start,kj,HN,dc,HN,end
-start,kj,HN,dc,end
-start,kj,HN,end
-start,kj,dc,HN,end
-start,kj,dc,end
-Finally, this even larger example has 226 paths through it:
-
-fs-end
-he-DX
-fs-he
-start-DX
-pj-DX
-end-zg
-zg-sl
-zg-pj
-pj-he
-RW-he
-fs-DX
-pj-RW
-zg-RW
-start-pj
-he-WI
-zg-he
-pj-fs
-start-RW
-How many paths through this cave system are there that visit small caves at most once?
+Given these new rules, how many paths through this cave system are there?
 """
 
 from util.timer import Timer
@@ -131,13 +82,11 @@ def convert_sensor_reading_to_dictionary(sensor_graph):
         'b',
       ]
       "A": [
-        'start',
         'b',
         'c',
         'end',
       ]
       "b": [
-        'start',
         'A',
         'd',
         'end',
@@ -153,9 +102,21 @@ def convert_sensor_reading_to_dictionary(sensor_graph):
     sensor_links = defaultdict(set)
     for sensor_reading in sensor_graph:
         parts = sensor_reading.split('-', -1)
-        sensor_links[parts[0]].add(parts[1])
-        if END_NODE != parts[1]:
+
+        if START_NODE == parts[0] or START_NODE == parts[1]:
+            if START_NODE == parts[0]:
+                sensor_links[parts[0]].add(parts[1])
+            else:
+                sensor_links[parts[1]].add(parts[0])
+        elif END_NODE == parts[0] or END_NODE == parts[1]:
+            if END_NODE == parts[0]:
+                sensor_links[parts[1]].add(parts[0])
+            else:
+                sensor_links[parts[0]].add(parts[1])
+        else:
+            sensor_links[parts[0]].add(parts[1])
             sensor_links[parts[1]].add(parts[0])
+
     return sensor_links
 
 
@@ -167,7 +128,9 @@ def crawl_next(graph, path, mutable_paths):
 
     if len(valid_links) == 0:
         if END_NODE == path[-1]:
-            mutable_paths.append(path)
+            t = tuple(path)
+            if t not in mutable_paths:
+                mutable_paths.add(t)
         return
 
     for node in valid_links:
@@ -186,12 +149,38 @@ def valid_paths(graph, path, node):
         return valid_paths
 
     node_paths = graph[node]
+
     for node_path in node_paths:
         if node_path.isupper():
             valid_paths.append(node_path)
+
         if node_path.islower() and node_path not in path:
             valid_paths.append(node_path)
+
+        has_2_lowers_already = False
+        for item in path:
+            if item.islower() and path.count(item) == 2:
+                has_2_lowers_already = True
+
+        if not has_2_lowers_already:
+            valid_paths.append(node_path)
+
     return valid_paths
+
+
+def dedupe_list(final_paths):
+    """
+    Given a list of lists, iterate through determining whether we have already seen to list and if so ignore it...
+    Converting to a tuple as list is not hashable whereas a tuple is.
+    """
+    seen = set()
+    deduped_list = []
+    for item in final_paths:
+        t = tuple(item)
+        if t not in seen:
+            deduped_list.append(item)
+            seen.add(t)
+    return deduped_list
 
 
 def process_sensors(sensor_graph):
@@ -200,10 +189,11 @@ def process_sensors(sensor_graph):
     """
     start_links = sensor_graph[START_NODE]
     # horrible but works
-    final_paths = []
+    final_paths = set()
     for sensor in start_links:
         start_path = [START_NODE, sensor]
         crawl_next(sensor_graph, start_path, final_paths)
+
     return final_paths
 
 
@@ -231,7 +221,7 @@ def process_file(filename, expected_result, print_result):
 
 
 if __name__ == '__main__':
-    process_file('input_example01.txt', 10, False)
-    process_file('input_example02.txt', 19, False)
-    process_file('input_example03.txt', 226, False)
-    process_file('input.txt', 149385, False)
+    process_file('input_example01.txt', 36, True)
+    process_file('input_example02.txt', 103, False)
+    process_file('input_example03.txt', 3509, False)
+    process_file('input.txt', 5254, False)
