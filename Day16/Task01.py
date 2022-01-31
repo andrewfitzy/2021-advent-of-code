d08d753239bc880fe409122f0da98b752cf7682a
Andrew Fitzpatrick <andrew.fitzpatrick@clicktravel.com>
2022-01-31 16:11:36 +0000

    Adding day16 skeleton stuff

--- Day16/Task01.py
+++ Day16/Task01.py
@@ -2,9 +2,12 @@
 --- Day 16: Packet Decoder ---
 As you leave the cave and reach open waters, you receive a transmission from the Elves back on the ship.
 
-The transmission was sent using the Buoyancy Interchange Transmission System (BITS), a method of packing numeric expressions into a binary sequence. Your submarine's computer has saved the transmission in hexadecimal (your puzzle input).
+The transmission was sent using the Buoyancy Interchange Transmission System (BITS), a method of packing numeric
+expressions into a binary sequence. Your submarine's computer has saved the transmission in hexadecimal (your puzzle
+input).
 
-The first step of decoding the message is to convert the hexadecimal representation into binary. Each character of hexadecimal corresponds to four bits of binary data:
+The first step of decoding the message is to convert the hexadecimal representation into binary. Each character of
+hexadecimal corresponds to four bits of binary data:
 
 0 = 0000
 1 = 0001
@@ -22,11 +25,18 @@ C = 1100
 D = 1101
 E = 1110
 F = 1111
-The BITS transmission contains a single packet at its outermost layer which itself contains many other packets. The hexadecimal representation of this packet might encode a few extra 0 bits at the end; these are not part of the transmission and should be ignored.
+The BITS transmission contains a single packet at its outermost layer which itself contains many other packets. The
+hexadecimal representation of this packet might encode a few extra 0 bits at the end; these are not part of the
+transmission and should be ignored.
 
-Every packet begins with a standard header: the first three bits encode the packet version, and the next three bits encode the packet type ID. These two values are numbers; all numbers encoded in any packet are represented as binary with the most significant bit first. For example, a version encoded as the binary sequence 100 represents the number 4.
+Every packet begins with a standard header: the first three bits encode the packet version, and the next three bits
+encode the packet type ID. These two values are numbers; all numbers encoded in any packet are represented as binary
+with the most significant bit first. For example, a version encoded as the binary sequence 100 represents the number 4.
 
-Packets with type ID 4 represent a literal value. Literal value packets encode a single binary number. To do this, the binary number is padded with leading zeroes until its length is a multiple of four bits, and then it is broken into groups of four bits. Each group is prefixed by a 1 bit except the last group, which is prefixed by a 0 bit. These groups of five bits immediately follow the packet header. For example, the hexadecimal string D2FE28 becomes:
+Packets with type ID 4 represent a literal value. Literal value packets encode a single binary number. To do this, the
+binary number is padded with leading zeroes until its length is a multiple of four bits, and then it is broken into
+groups of four bits. Each group is prefixed by a 1 bit except the last group, which is prefixed by a 0 bit. These
+groups of five bits immediately follow the packet header. For example, the hexadecimal string D2FE28 becomes:
 
 110100101111111000101000
 VVVTTTAAAAABBBBBCCCCC
@@ -34,52 +44,183 @@ Below each bit is a label indicating its purpose:
 
 The three bits labeled V (110) are the packet version, 6.
 The three bits labeled T (100) are the packet type ID, 4, which means the packet is a literal value.
-The five bits labeled A (10111) start with a 1 (not the last group, keep reading) and contain the first four bits of the number, 0111.
-The five bits labeled B (11110) start with a 1 (not the last group, keep reading) and contain four more bits of the number, 1110.
-The five bits labeled C (00101) start with a 0 (last group, end of packet) and contain the last four bits of the number, 0101.
+The five bits labeled A (10111) start with a 1 (not the last group, keep reading) and contain the first four bits of
+the number, 0111.
+The five bits labeled B (11110) start with a 1 (not the last group, keep reading) and contain four more bits of the
+number, 1110.
+The five bits labeled C (00101) start with a 0 (last group, end of packet) and contain the last four bits of the number,
+0101.
 The three unlabeled 0 bits at the end are extra due to the hexadecimal representation and should be ignored.
 So, this packet represents a literal value with binary representation 011111100101, which is 2021 in decimal.
 
-Every other type of packet (any packet with a type ID other than 4) represent an operator that performs some calculation on one or more sub-packets contained within. Right now, the specific operations aren't important; focus on parsing the hierarchy of sub-packets.
+Every other type of packet (any packet with a type ID other than 4) represent an operator that performs some
+calculation on one or more sub-packets contained within. Right now, the specific operations aren't important; focus on
+parsing the hierarchy of sub-packets.
 
-An operator packet contains one or more packets. To indicate which subsequent binary data represents its sub-packets, an operator packet can use one of two modes indicated by the bit immediately after the packet header; this is called the length type ID:
+An operator packet contains one or more packets. To indicate which subsequent binary data represents its sub-packets,
+an operator packet can use one of two modes indicated by the bit immediately after the packet header; this is called
+the length type ID:
 
-If the length type ID is 0, then the next 15 bits are a number that represents the total length in bits of the sub-packets contained by this packet.
-If the length type ID is 1, then the next 11 bits are a number that represents the number of sub-packets immediately contained by this packet.
+If the length type ID is 0, then the next 15 bits are a number that represents the total length in bits of the
+sub-packets contained by this packet.
+If the length type ID is 1, then the next 11 bits are a number that represents the number of sub-packets immediately
+contained by this packet.
 Finally, after the length type ID bit and the 15-bit or 11-bit field, the sub-packets appear.
 
-For example, here is an operator packet (hexadecimal string 38006F45291200) with length type ID 0 that contains two sub-packets:
+For example, here is an operator packet (hexadecimal string 38006F45291200) with length type ID 0 that contains two
+sub-packets:
 
 00111000000000000110111101000101001010010001001000000000
 VVVTTTILLLLLLLLLLLLLLLAAAAAAAAAAABBBBBBBBBBBBBBBB
 The three bits labeled V (001) are the packet version, 1.
 The three bits labeled T (110) are the packet type ID, 6, which means the packet is an operator.
-The bit labeled I (0) is the length type ID, which indicates that the length is a 15-bit number representing the number of bits in the sub-packets.
+The bit labeled I (0) is the length type ID, which indicates that the length is a 15-bit number representing the number
+of bits in the sub-packets.
 The 15 bits labeled L (000000000011011) contain the length of the sub-packets in bits, 27.
 The 11 bits labeled A contain the first sub-packet, a literal value representing the number 10.
 The 16 bits labeled B contain the second sub-packet, a literal value representing the number 20.
-After reading 11 and 16 bits of sub-packet data, the total length indicated in L (27) is reached, and so parsing of this packet stops.
+After reading 11 and 16 bits of sub-packet data, the total length indicated in L (27) is reached, and so parsing of
+this packet stops.
 
-As another example, here is an operator packet (hexadecimal string EE00D40C823060) with length type ID 1 that contains three sub-packets:
+As another example, here is an operator packet (hexadecimal string EE00D40C823060) with length type ID 1 that contains
+three sub-packets:
 
 11101110000000001101010000001100100000100011000001100000
 VVVTTTILLLLLLLLLLLAAAAAAAAAAABBBBBBBBBBBCCCCCCCCCCC
 The three bits labeled V (111) are the packet version, 7.
 The three bits labeled T (011) are the packet type ID, 3, which means the packet is an operator.
-The bit labeled I (1) is the length type ID, which indicates that the length is a 11-bit number representing the number of sub-packets.
+The bit labeled I (1) is the length type ID, which indicates that the length is a 11-bit number representing the
+number of sub-packets.
 The 11 bits labeled L (00000000011) contain the number of sub-packets, 3.
 The 11 bits labeled A contain the first sub-packet, a literal value representing the number 1.
 The 11 bits labeled B contain the second sub-packet, a literal value representing the number 2.
 The 11 bits labeled C contain the third sub-packet, a literal value representing the number 3.
-After reading 3 complete sub-packets, the number of sub-packets indicated in L (3) is reached, and so parsing of this packet stops.
+After reading 3 complete sub-packets, the number of sub-packets indicated in L (3) is reached, and so parsing of this
+packet stops.
 
 For now, parse the hierarchy of the packets throughout the transmission and add up all of the version numbers.
 
 Here are a few more examples of hexadecimal-encoded transmissions:
 
-8A004A801A8002F478 represents an operator packet (version 4) which contains an operator packet (version 1) which contains an operator packet (version 5) which contains a literal value (version 6); this packet has a version sum of 16.
-620080001611562C8802118E34 represents an operator packet (version 3) which contains two sub-packets; each sub-packet is an operator packet that contains two literal values. This packet has a version sum of 12.
-C0015000016115A2E0802F182340 has the same structure as the previous example, but the outermost packet uses a different length type ID. This packet has a version sum of 23.
-A0016C880162017C3686B18A3D4780 is an operator packet that contains an operator packet that contains an operator packet that contains five literal values; it has a version sum of 31.
-Decode the structure of your hexadecimal-encoded BITS transmission; what do you get if you add up the version numbers in all packets?
+8A004A801A8002F478 represents an operator packet (version 4) which contains an operator packet (version 1) which
+    contains an operator packet (version 5) which contains a literal value (version 6); this packet has a version sum of 16.
+620080001611562C8802118E34 represents an operator packet (version 3) which contains two sub-packets; each sub-packet
+    is an operator packet that contains two literal values. This packet has a version sum of 12.
+C0015000016115A2E0802F182340 has the same structure as the previous example, but the outermost packet uses a different
+    length type ID. This packet has a version sum of 23.
+A0016C880162017C3686B18A3D4780 is an operator packet that contains an operator packet that contains an operator packet
+    that contains five literal values; it has a version sum of 31.
+
+Decode the structure of your hexadecimal-encoded BITS transmission; what do you get if you add up the version numbers
+in all packets?
 """
+from util.timer import Timer
+# import abc
+#
+# class Type(metaclass=abc.ABCMeta):
+#     @abc.abstractmethod
+#     def process_type(self):
+#         pass
+#
+#
+# class Type04(Type):
+#     pass
+
+
+def get_input(filename):
+    """
+    Takes a filename and returns a list of lines from the file
+    """
+    output = []
+    with open(filename) as f:
+        for line in f:
+            tmp_line = line.strip()
+            output.append(tmp_line)
+    return output
+
+
+def get_version(input_as_binary):
+    tmp_version = input_as_binary.pop()
+    print(tmp_version)
+    return 1
+
+
+def get_type(input_as_binary):
+    return 1
+
+
+def process_reading(input_from_file):
+    '''
+    convert to binary
+    '''
+    input_as_integer = int(input_from_file, 16)
+    input_as_binary = bin(input_as_integer)[2:]
+    input_as_binary_list = list(input_as_binary)
+    while len(input_as_binary_list) != 0:
+        version = input_as_binary_list[:3]
+        input_as_binary_list = input_as_binary_list[3:]
+
+        type = input_as_binary_list[:3]
+        input_as_binary_list = input_as_binary_list[3:]
+
+        decimal_version = int(''.join(version),2)
+        decimal_type = int(''.join(type), 2)
+        print('Type: ' + str(decimal_type) + ' Version: ' + str(decimal_version))
+
+        print(decimal_type)
+        if decimal_type == 1:
+            print('Type 1')
+        elif decimal_type == 4:
+            prefix = 1
+            while prefix == 1:
+                literal = input_as_binary_list[:5]
+                print(literal)
+                input_as_binary_list = input_as_binary_list[5:]
+
+                prefix = literal[0]
+        else:
+            print('UNKNOWN TYPE: ' + str(type))
+
+        '''
+        need to iterate through the number, popping
+        get 3 chars, this is version
+        put version in a list
+        get 3 chars, this is type
+            process type X - nasty ifs but will do the job - would usually invert... actually no, will create classes with
+            process and a class for each type.
+            process type y etc etc
+        do we care about the rest... understand and popuntil the next version
+        '''
+    print(input_as_binary)
+    return 2012
+
+
+def process_file(filename, expected_result, print_result):
+    """
+    Given a file and an expected result, process the file and see if the result is as expected
+    """
+    t = Timer()
+    t.start()
+    input_from_file = get_input(filename)
+
+    assert len(input_from_file) == 1, 'Input ' + filename + ' should contain only one line'
+
+    result = process_reading(input_from_file[0])
+
+    if print_result:
+        print('result')
+
+    result = 0
+    assert result == expected_result, 'Output is not as expected, expected ' + str(expected_result) + ' but got ' + str(result)
+
+    print('RESULT: there are ' + str(result) + ' paths for input ' + filename)
+    t.stop()
+
+
+if __name__ == '__main__':
+    process_file('input_example00.txt', 16, False)
+    process_file('input_example01.txt', 16, False)
+    process_file('input_example02.txt', 12, False)
+    process_file('input_example03.txt', 23, False)
+    process_file('input_example04.txt', 31, False)
+    process_file('input.txt', 1, False)
