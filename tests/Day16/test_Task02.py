38caca764848f5ff4c909c9f6027eb2d0605cb27
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2025-02-15 08:09:00 +0000

    Adding day 16 pt 2

--- tests/Day16/test_Task02.py
+++ tests/Day16/test_Task02.py
@@ -9,8 +9,116 @@ from Day16.Task02 import solve
 from tests.test_utils.get_input import get_input
 
 
-def test_example_input():
+def test_example_input_01():
     file_content = get_input("tests/Day16/input_example01.txt")
+
+    expected = 2021
+
+    result = solve(file_content)
+    assert result == expected
+
+
+def test_example_input_02():
+    file_content = get_input("tests/Day16/input_example02.txt")
+
+    expected = 15
+
+    result = solve(file_content)
+    assert result == expected
+
+
+def test_example_input_03():
+    file_content = get_input("tests/Day16/input_example03.txt")
+
+    expected = 46
+
+    result = solve(file_content)
+    assert result == expected
+
+
+def test_example_input_04():
+    file_content = get_input("tests/Day16/input_example04.txt")
+
+    expected = 46
+
+    result = solve(file_content)
+    assert result == expected
+
+
+def test_example_input_05():
+    file_content = get_input("tests/Day16/input_example05.txt")
+
+    expected = 54
+
+    result = solve(file_content)
+    assert result == expected
+
+
+def test_example_input_06():
+    file_content = get_input("tests/Day16/input_example06.txt")
+
+    expected = 3
+
+    result = solve(file_content)
+    assert result == expected
+
+
+def test_example_input_07():
+    file_content = get_input("tests/Day16/input_example07.txt")
+
+    expected = 54
+
+    result = solve(file_content)
+    assert result == expected
+
+
+def test_example_input_08():
+    file_content = get_input("tests/Day16/input_example08.txt")
+
+    expected = 7
+
+    result = solve(file_content)
+    assert result == expected
+
+
+def test_example_input_09():
+    file_content = get_input("tests/Day16/input_example09.txt")
+
+    expected = 9
+
+    result = solve(file_content)
+    assert result == expected
+
+
+def test_example_input_10():
+    file_content = get_input("tests/Day16/input_example10.txt")
+
+    expected = 1
+
+    result = solve(file_content)
+    assert result == expected
+
+
+def test_example_input_11():
+    file_content = get_input("tests/Day16/input_example11.txt")
+
+    expected = 0
+
+    result = solve(file_content)
+    assert result == expected
+
+
+def test_example_input_12():
+    file_content = get_input("tests/Day16/input_example12.txt")
+
+    expected = 0
+
+    result = solve(file_content)
+    assert result == expected
+
+
+def test_example_input_13():
+    file_content = get_input("tests/Day16/input_example13.txt")
 
     expected = 1
 
@@ -24,7 +132,7 @@ def test_example_input():
 def test_real_input():
     file_content = get_input("tests/Day16/input.txt")
 
-    expected = 1
+    expected = 1264485568252
 
     result = solve(file_content)
     assert result == expected
