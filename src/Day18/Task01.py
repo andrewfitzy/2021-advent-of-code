0303b239c0078c9ea5ceef069504cbf674d9c2f7
Andrew Worden-Fitzpatrick <andrewpfitzpatrick@gmail.com>
2026-02-05 20:22:34 +0000

    day 18 part 1 and 2

---
+++ src/Day18/Task01.py
@@ -0,0 +1,189 @@
+# Standard Library
+import re
+
+PAIR_PATTERN = re.compile("\\[[0-9]+,[0-9]+\\]")
+MULTIDIGIT_PATTERN = re.compile("[0-9]{2,}")
+WHOLE_NUMBER_PATTERN = re.compile("[0-9]+")
+
+
+def solve(file_content):
+    previous_answer = file_content[0]
+    for index in range(1, len(file_content)):
+        next_sum = add(previous_answer, file_content[index])
+        next_sum = reduce(next_sum)
+        previous_answer = next_sum
+    return calculate_magnitude(previous_answer)
+
+
+def add(input01, input02):
+    return "[{input01},{input02}]".format(input01=input01, input02=input02)
+
+
+def reduce(input):
+    input_copy = str(input)
+
+    # repeat reduce until the input "number" is fully reduced
+    fully_reduced = False
+    while not fully_reduced:
+        # check if it needs exploding, explode once and then continue
+        if needs_exploding(input_copy):
+            input_copy = explode(input_copy)
+            continue
+
+        # check if it needs splitting, split once and then continue
+        if needs_splitting(input_copy):
+            input_copy = split(input_copy)
+            continue
+
+        # If we get here we didn't need to explode and didn't need to split so we're reduced
+        fully_reduced = True
+
+    return input_copy
+
+
+def needs_exploding(input):
+    # go through, if we get to 5 items in the stack we explode
+    counter = 0
+    for character in input:
+        if counter >= 5:
+            return True
+        if "[" == character:
+            counter = counter + 1
+        if "]" == character:
+            counter = counter - 1
+
+    return False
+
+
+def add_to_previous_num(number, input):
+    # find next number
+    match = None
+    for match in WHOLE_NUMBER_PATTERN.finditer(input):
+        continue
+
+    if not match:
+        return input
+
+    to_insert = int(match.group()) + int(number)
+
+    left = input[: match.start()]
+    right = input[match.end() :]
+
+    result = "{left}{to_insert}{right}".format(left=left, to_insert=to_insert, right=right)
+
+    return result
+
+
+def add_to_next_num(number, input):
+    # find next number
+    match = None
+    for match in WHOLE_NUMBER_PATTERN.finditer(input):
+        break
+
+    if not match:
+        return input
+
+    to_insert = int(match.group()) + int(number)
+
+    left = input[: match.start()]
+    right = input[match.end() :]
+
+    result = "{left}{to_insert}{right}".format(left=left, to_insert=to_insert, right=right)
+
+    return result
+
+
+def explode(input):
+    # go through, explode left most match
+    counter = 0
+    pointer = 0
+    for character in input:
+        if counter >= 5:
+            end_pointer = input.find("]", pointer)
+
+            # need to minus and plus 1 to capture the []
+            start = pointer - 1
+            end = end_pointer + 1
+
+            # check that it's a range, if so break, else continue,
+            check_range = input[start:end]
+
+            if PAIR_PATTERN.match(check_range):
+                explode_range = (start, end)
+                break
+        if "[" == character:
+            counter = counter + 1
+        if "]" == character:
+            counter = counter - 1
+        pointer = pointer + 1
+
+    if not explode_range:
+        raise Exception("Can't find the number that needs exploding")
+
+    left = input[: explode_range[0]]
+    right = input[explode_range[1] :]
+
+    pair_left, pair_right = input[explode_range[0] : explode_range[1]].replace("[", "").replace("]", "").split(",")
+
+    result_left = add_to_previous_num(pair_left, left)
+    result_right = add_to_next_num(pair_right, right)
+
+    # concat
+    result = "{result_left}0{result_right}".format(result_left=result_left, result_right=result_right)
+
+    #  now how to explode?
+    return result
+
+
+def needs_splitting(input):
+    # need to find consecutive numbers and check if they are greater than 10
+    result = MULTIDIGIT_PATTERN.search(input)
+    if result:
+        return True
+    return False
+
+
+def split(input):
+    # go through, split left most match
+    result = MULTIDIGIT_PATTERN.search(input)
+    if not result:
+        raise Exception("Can't find the number that needs splitting")
+
+    # get the number to split as an int then perform the maths
+    number_to_split = int(input[result.start() : result.end()])
+    left = number_to_split // 2
+    right = number_to_split - left
+
+    # get the right and left strings around the original number
+    start = input[: result.start()]
+    end = input[result.end() :]
+
+    # concat it all together and return
+    new_string = "{start}[{left},{right}]{end}".format(start=start, left=left, right=right, end=end)
+    return new_string
+
+
+def calculate_magnitude(input):
+    # find pairs
+    pairs = list(PAIR_PATTERN.finditer(input))
+
+    # if not a pair, it's a number so return it
+    if not pairs:
+        return int(input)
+
+    # for each pair, starting from end
+    for pair in pairs[::-1]:
+        # calculate the magnitude
+        left, right = pair.group().replace("[", "").replace("]", "").split(",")
+        value = int(left) * 3 + int(right) * 2
+
+        # find where our pair was
+        start = input[: pair.start()]
+        end = input[pair.end() :]
+
+        # replace our pair with our number
+        input = "{start}{value}{end}".format(start=start, value=value, end=end)
+
+        return calculate_magnitude(input)
+
+    raise ValueError(f"cannot parse snailfish number: {input}")
---
+++ tests/Day18/test_Task01.py
@@ -0,0 +1,202 @@
+# Standard Library
+import os
+
+# Dependencies
+import pytest
+
+# From apps
+from Day18.Task01 import add, calculate_magnitude, explode, needs_exploding, needs_splitting, solve, split
+from tests.test_utils.get_input import get_input
+
+
+def test_add():
+    input01 = "[1,2]"
+    input02 = "[[3,4],5]"
+
+    expected = "[[1,2],[[3,4],5]]"
+
+    result = add(input01, input02)
+    assert result == expected
+
+
+def test_needs_exploding_true():
+    input = "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+
+    expected = True
+
+    result = needs_exploding(input)
+    assert result == expected
+
+
+def test_needs_exploding_false():
+    input = "[[3,[2,[8,0]]],[9,[5,[4,0]]]]"
+
+    expected = False
+
+    result = needs_exploding(input)
+    assert result == expected
+
+
+def test_explode_01():
+    input = "[[[[[9,8],1],2],3],4]"
+
+    expected = "[[[[0,9],2],3],4]"
+
+    result = explode(input)
+    assert result == expected
+
+
+def test_explode_02():
+    input = "[7,[6,[5,[4,[3,2]]]]]"
+
+    expected = "[7,[6,[5,[7,0]]]]"
+
+    result = explode(input)
+    assert result == expected
+
+
+def test_explode_03():
+    input = "[[6,[5,[4,[3,2]]]],1]"
+
+    expected = "[[6,[5,[7,0]]],3]"
+
+    result = explode(input)
+    assert result == expected
+
+
+def test_explode_04():
+    input = "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"
+
+    expected = "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+
+    result = explode(input)
+    assert result == expected
+
+
+def test_explode_05():
+    input = "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+
+    expected = "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
+
+    result = explode(input)
+    assert result == expected
+
+
+def test_needs_splitting_true():
+    input = "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"
+
+    expected = True
+
+    result = needs_splitting(input)
+    assert result == expected
+
+
+def test_needs_splitting_false():
+    input = "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+
+    expected = False
+
+    result = needs_splitting(input)
+    assert result == expected
+
+
+def test_split_01():
+    input = "[[[[0,7],4],[15,[0,13]]],[1,1]]"
+
+    expected = "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"
+
+    result = split(input)
+    assert result == expected
+
+
+def test_split_02():
+    input = "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"
+
+    expected = "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]"
+
+    result = split(input)
+    assert result == expected
+
+
+def test_calculate_magnitude_01():
+    input = "[[1,2],[[3,4],5]]"
+
+    expected = 143
+
+    result = calculate_magnitude(input)
+    assert result == expected
+
+
+def test_calculate_magnitude_02():
+    input = "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
+
+    expected = 1384
+
+    result = calculate_magnitude(input)
+    assert result == expected
+
+
+def test_calculate_magnitude_03():
+    input = "[[[[1,1],[2,2]],[3,3]],[4,4]]"
+
+    expected = 445
+
+    result = calculate_magnitude(input)
+    assert result == expected
+
+
+def test_calculate_magnitude_04():
+    input = "[[[[3,0],[5,3]],[4,4]],[5,5]]"
+
+    expected = 791
+
+    result = calculate_magnitude(input)
+    assert result == expected
+
+
+def test_calculate_magnitude_05():
+    input = "[[[[5,0],[7,4]],[5,5]],[6,6]]"
+
+    expected = 1137
+
+    result = calculate_magnitude(input)
+    assert result == expected
+
+
+def test_calculate_magnitude_06():
+    input = "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"
+
+    expected = 3488
+
+    result = calculate_magnitude(input)
+    assert result == expected
+
+
+def test_example_input01():
+    file_content = get_input("tests/Day18/input_example01.txt")
+
+    expected = 3488
+
+    result = solve(file_content)
+    assert result == expected
+
+
+def test_example_input02():
+    file_content = get_input("tests/Day18/input_example02.txt")
+
+    expected = 4140
+
+    result = solve(file_content)
+    assert result == expected
+
+
+@pytest.mark.skipif(
+    os.environ["TEST_ENV"] == "staging", reason="My input file is not added to git, only run this locally"
+)
+def test_real_input():
+    file_content = get_input("tests/Day18/input.txt")
+
+    expected = 4145
+
+    result = solve(file_content)
+    assert result == expected
