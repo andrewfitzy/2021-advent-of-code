fd5800928e36f6a1ed2f4e1864bd93f12e89840d
Andrew Worden-Fitzpatrick <andrewpfitzpatrick@gmail.com>
2026-02-08 15:55:14 +0000

    Adding day 21 pt 2 and corrected part 1

--- src/Day21/Task01.py
+++ src/Day21/Task01.py
@@ -1,35 +1,28 @@
-game = {"0": {"position": 0, "score": 0}, "1": {"position": 0, "score": 0}}
-
-die = {
-    "numberOfRolls": 0,
-    "lastRoll": 0,
-}
-
-
-def is_won(winning_mark):
-    for player, game_state in game.items():
+def is_won(game, winning_mark):
+    for _, game_state in game.items():
         if game_state["score"] >= winning_mark:
             return True
     return False
 
 
-def get_moves():
+def get_moves(die):
     i = 0
     rolls_total = 0
     while i < 3:
         i += 1
         die["numberOfRolls"] += 1
         die["lastRoll"] += 1
-        if die["lastRoll"] == 101:
+        if die["lastRoll"] > 100:
             die["lastRoll"] = 1
         rolls_total += die["lastRoll"]
     return rolls_total
 
 
-def play_game(winning_mark):
+def play_game(game, die, winning_mark):
     i = 0
+
     while True:
-        moves = get_moves()
+        moves = get_moves(die)
 
         player = i % 2
 
@@ -40,7 +33,7 @@ def play_game(winning_mark):
 
         game[str(player)]["score"] += game[str(player)]["position"]
 
-        if is_won(winning_mark):
+        if is_won(game, winning_mark):
             winner = player
             loser = 0 if winner == 1 else 1
             break
@@ -52,13 +45,21 @@ def play_game(winning_mark):
     }
 
 
-def get_score(loser):
+def get_score(game, die, loser):
     return game[str(loser)]["score"] * die["numberOfRolls"]
 
 
 def solve(file_content):
-    game["0"]["position"] = int(file_content[0].split(" ")[4])
-    game["1"]["position"] = int(file_content[1].split(" ")[4])
-    result = play_game(1000)
-    loser_score = get_score(result["loser"])
+    game = {
+        "0": {"position": int(file_content[0].split(" ")[4]), "score": 0},
+        "1": {"position": int(file_content[1].split(" ")[4]), "score": 0},
+    }
+
+    die = {
+        "numberOfRolls": 0,
+        "lastRoll": 0,
+    }
+
+    result = play_game(game, die, 1000)
+    loser_score = get_score(game, die, result["loser"])
     return loser_score
--- tests/Day21/test_Task01.py
+++ tests/Day21/test_Task01.py
@@ -11,6 +11,7 @@ from tests.test_utils.get_input import get_input
 
 def test_example_input():
     file_content = get_input("tests/Day21/input_example.txt")
+
     expected = 739785
 
     result = solve(file_content)
@@ -23,9 +24,7 @@ def test_example_input():
 def test_real_input():
     file_content = get_input("tests/Day21/input.txt")
 
-    # 2 years later this gives a different result, it should be 742257, not sure why
-    # setting to the wrong value to make the test pass
-    expected = 742020
+    expected = 742257
 
     result = solve(file_content)
     assert result == expected
